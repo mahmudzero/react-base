4f8f937672bcca8f90322d3fde1ac8831fb1a254
Mahmud Ahmad <ahmad.mahmud1997@gmail.com>
2019-09-12 23:01:57 -0400

    added react and babel source to react-src

--- main.jsx
+++ main.jsx
@@ -10,7 +10,36 @@ class NavBar extends React.Component {
 
   render() {
     return(
-      <nav className={this.props.className}> AppName </nav>
+      <nav className='nav-bar'>
+        <div> ReactBase </div>
+        <div className='logo'> // </div>
+        <div> Mahmud Ahmad </div>
+      </nav>
+    );
+  };
+};
+
+class PagePreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author_name: 'Johnny AppleSeed',
+      author_image: '',
+      description: '',
+    };
+  };
+
+  render() {
+    return(
+      <div className='page-preview'>
+        <div className='page-preview__name-image-container'>
+          <div className='page-preview__image'> </div>
+          <div className='page-preview__name'> Johnny Appleseed </div>
+        </div>
+        <div className='page-preview__description-container'>
+          Qui aute aliqua est voluptate et sit proident. Consectetur velit tempor duis laborum. Velit ad eiusmod enim tempor ex id aliqua dolor id eiusmod aliqua. 
+        </div>
+      </div>
     );
   };
 };
@@ -24,18 +53,23 @@ class App extends React.Component {
   };
 
   render() {
+    // we can render arrays of components
+    // const _nav_bar = [<NavBar key='1'/>, <NavBar key='2'/>];
+    const _page_prev = [<PagePreview key='1' />, <PagePreview key='2' />, <PagePreview key='3' />, <PagePreview key='4' />];
     return(
       <div className='app'>
-        <NavBar className='nav-bar'/>
-        <div className='hello-world'> Hello world, my name is {this.props.name||'Null'} </div>
+        <NavBar />
+        <div className='add-new-dom'> + New DOM </div>
+        {/* as seen here */}
+        {/* { _nav_bar } */}
+        <div class='page-preview-container'>
+          <PagePreview />
+          { _page_prev }
+        </div>
       </div>
     );
   };
 };
 
-
-
-
-const _object = <App name='App'/* props */ />;
-
-ReactDOM.render(_object, document.getElementById('root'));
+const _app = <App name='App'/* props */ />;
+ReactDOM.render(_app, document.getElementById('root'));
