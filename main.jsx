6d28ea375625aaac98faf70be6db948778e5afe7
Mahmud Ahmad <ahmad.mahmud1997@gmail.com>
2019-09-13 21:53:09 -0400

    commit before i mess things up..
    more ui updates

--- main.jsx
+++ main.jsx
@@ -24,23 +24,42 @@ class PagePreview extends React.Component {
     super(props);
     this.state = {
       author_name: 'Johnny AppleSeed',
-      author_image: '',
-      description: '',
+      description: 'Duis eiusmod id adipisicing sunt id.',
     };
   };
 
   render() {
-    return(
-      <div className='page-preview'>
+    const name = this.props.name || this.state.author_name;
+    const _first_initial = name[0];
+    if(!this.props.img_src) {
+      return(
+        <div className='page-preview'>
+          <div className='page-preview__name-image-container'>
+            <div className='page-preview__image-container'>
+              { _first_initial }
+            </div>
+            <div className='page-preview__name'> { this.props.name || this.state.author_name } </div>
+          </div>
+          <div className='page-preview__description-container'>
+            { this.props.description || this.state.description }
+          </div>
+        </div>
+      );
+    } else {
+      return(
+        <div className='page-preview'>
         <div className='page-preview__name-image-container'>
-          <div className='page-preview__image'> </div>
-          <div className='page-preview__name'> Johnny Appleseed </div>
+          <div className='page-preview__image-container'>
+            <img className='page-preview__image' src={this.props.img_src}/>
+          </div>
+          <div className='page-preview__name'> { this.props.name || this.state.author_name } </div>
         </div>
         <div className='page-preview__description-container'>
-          Qui aute aliqua est voluptate et sit proident. Consectetur velit tempor duis laborum. Velit ad eiusmod enim tempor ex id aliqua dolor id eiusmod aliqua. 
+          { this.props.description || this.state.description }
         </div>
       </div>
-    );
+      );
+    }
   };
 };
 
@@ -52,6 +71,41 @@ class App extends React.Component {
     };
   };
 
+  _render_page_prev = () => {
+    const _page_prev_json = [
+      {
+        name: "Johnny Appleseed",
+        img_src: "",
+        description: "Ad ea ea velit aliqua nostrud aliquip aute commodo ut sit nostrud duis sit ad. Minim cupidatat sunt reprehenderit minim sit anim nulla magna amet non laboris dolor commodo nisi.",
+      }, {
+        name: "Mufasa Waititi",
+        img_src: "",
+        description: "Anim pariatur anim incididunt enim deserunt quis excepteur laborum dolore.",
+      }, {
+        name: "Eddie Hall",
+        img_src: "https://www.animatedtimes.com/wp-content/uploads/2019/07/thorragnarok-jeffgoldblum-grandmaster-uneasy-700x334-1280x720.jpg",
+        description: "Eiusmod dolore ullamco mollit eiusmod excepteur qui in eiusmod.",
+      }, {
+        name: "",
+        img_src: "",
+        description: "",
+      }
+    ];
+    const _rendered = _page_prev_json.map((datum, i) => {
+      return (
+        <PagePreview 
+          name={datum.name} 
+          img_src={datum.img_src}
+          description={datum.description}
+          key={i}
+        />
+      );
+    });
+    return ( 
+      _rendered
+    );
+  }
+
   render() {
     // we can render arrays of components
     // const _nav_bar = [<NavBar key='1'/>, <NavBar key='2'/>];
@@ -62,9 +116,10 @@ class App extends React.Component {
         <div className='add-new-dom'> + New DOM </div>
         {/* as seen here */}
         {/* { _nav_bar } */}
-        <div class='page-preview-container'>
+        <div className='page-preview-container'>
           <PagePreview />
           { _page_prev }
+          { this._render_page_prev() }
         </div>
       </div>
     );
