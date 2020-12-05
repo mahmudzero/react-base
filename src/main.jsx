96abe149d0c6207ac192f34d0fb5558456c9dcfc
Mahmud Ahmad <ahmad.mahmud1997@gmail.com>
2020-12-05 17:12:44 -0500

    tabs -> spaces, functional component

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	};
+function App() {
+  /**
+   * example of using state
+   */
+  const [ex_state, set_ex_state] = React.useState({});
 
-	render() {
-		return (
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-					height: "100vh",
-					width: "100vw",
-				}}
-				className='app'
-			>
-				<div
-					style={{
-						color: "rgb(128, 180, 210)",
-						fontSize: "28px"
-					}}
-			>
-					Starter Project
-				</div>
-			</div>
-		);
-	};
-};
+  /**
+   * init hook
+   */
+  React.useEffect(
+    () => {
+      /**
+       * do stuff in here on init
+       */
+    },
+    [] // useEffect is called when things passed into the dependency list change value
+  );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+      className='app'
+    >
+      <div
+        style={{
+          color: "rgb(128, 180, 210)",
+          fontSize: "28px"
+        }}
+    >
+        Starter Project
+      </div>
+    </div>
+  );
+
+}
 
 const _app = <App name='App'/* props */ />;
 ReactDOM.render(_app, document.getElementById('root'));
